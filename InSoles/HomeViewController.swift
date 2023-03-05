08e6d620868751439dfa27a6c81cf3b899a1f684
kukucoolguy05 <Kurinjikumar2005@gmail.com>
2023-03-05 01:40:06 -0600

    first comment

--- InSoles/HomeViewController.swift
+++ InSoles/HomeViewController.swift
@@ -8,22 +8,35 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var homeButton2: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        homeButton.layer.cornerRadius = 15
+        homeButton2.layer.cornerRadius = 15
+    
+        let alert = UIAlertController(title: "What is your name", message: nil, preferredStyle: .alert)
+        present(alert,animated: true,completion: nil)
+        
+        alert.addTextField(configurationHandler: {textField in textField.placeholder =
+            "Your name"})
+        
+        let enter = UIAlertAction(title: "Enter", style: .default)
+        {
+            action in
+            if let name = alert.textFields![0].text {
+                self.nameLabel.text = "Hello, " + name
+            }
+        }
+        alert.addAction(enter)
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(cancel)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
