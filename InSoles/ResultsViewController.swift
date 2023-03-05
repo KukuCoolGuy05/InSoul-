7a659ed55425f7e6a8b8d732cb109157e310444e
kukucoolguy05 <Kurinjikumar2005@gmail.com>
2023-03-05 03:02:20 -0600

    commit 5

---
+++ InSoles/ResultsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ResultsViewController.swift
+//  InSoles
+//
+//  Created by user on 3/5/23.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
