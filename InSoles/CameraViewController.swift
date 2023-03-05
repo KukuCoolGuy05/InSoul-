eced8d319be68acc2688440a26dedd51ed39e3b1
kukucoolguy05 <Kurinjikumar2005@gmail.com>
2023-03-05 02:00:10 -0600

    commit 2

--- InSoles/CameraViewController.swift
+++ InSoles/CameraViewController.swift
@@ -7,12 +7,42 @@
 
 import UIKit
 
+
+
 class CameraViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        imageView.backgroundColor = .secondarySystemBackground
+        button.backgroundColor = .systemBlue
+        button.setTitle("Take Picture Of Your Feet!", for: .normal)
+        button.setTitleColor(.white, for: .normal)
     }
+    
+    @IBAction func didTapButton(){
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+}
 
-
+extension CameraViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage  else {
+            return
+        }
+        imageView.image = image
+    }
 }
